843c41cf3093202b444c72ae2ac4578364794a57
igorhf <igorhf.2@gmail.com>
2020-01-20 09:53:43 -0300

    adicionando app

---
+++ Signos/app/src/main/java/com/example/igor/signos/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.igor.signos;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private ListView lista;
+    private String[] signos = {
+            "Áries",
+            "Touro",
+            "Gêmeos",
+            "Câncer",
+            "Leão",
+            "Virgem",
+            "Libra",
+            "Escorpião",
+            "Sagitário",
+            "Capricórnio",
+            "Aquário",
+            "Peixes"
+    };
+
+    private String[] descricao = {
+            "Áries costuma viver no agora; além de possuir um grande prazer em existir, ação e decisão também podem ser algumas de suas marcas. A liderança é uma característica forte no signo, que pode ser usada para o empreendedorismo.",
+            "Touro costuma não abrir mão do que traz conforto e bem-estar, mas pode ter a possessividade em evidência. O signo preza pela beleza física e pela gula. A satisfação de Touro vem do contato com todos os cinco sentidos.",
+            "Gêmeos costuma apreciar novas ideias, novos lugares e novas pessoas. Gêmeos tem carisma de sobra e tende a fazer amigos com muita facilidade. A superficialidade e a inconsequência também podem ser algumas das marcas do signo.",
+            "Câncer costuma buscar o que lhe traz segurança. O signo é maternal e emotivo e tem uma proximidade com o apego ao passado. Câncer costuma ter a intuição acentuada, sentir as coisas no ar e captar tudo logo no primeiro olhar.",
+            "Leão costuma amar plateias e aplausos. O signo tem a coragem e o humor como marcas registradas, mas também o orgulho e o autoritarismo. Leão geralmente valoriza as oportunidades e aproveita cada momento, sabe amar e ter generosidade.",
+            "Virgem costuma buscar constantemente a perfeição, tudo sempre poderia estar um pouco melhor. Signo observador, que costuma identificar o detalhe que ninguém mais percebe. Virgem tem praticidade e organização, mas também pode ser frio e cético.\n" +
+                    "Elemento e ritmo: Terra Mutável",
+            "Libra costuma buscar sempre o equilíbrio e conviver com a indecisão. Regido por Vênus, o signo tem uma aproximação natural com as artes. Libra costuma ter ética, mas também pode ter vaidade em excesso e comportamentos considerados de “mau gosto”.",
+            "Escorpião costuma ter muita perspicácia, não é a toa que são naturalmente detetives. O sexto sentido do signo alerta para possíveis “perigos” logo de cara. Escorpião tem uma capacidade de regeneração única, mas também pode ter muitos ciúmes.",
+            "Sagitário costuma ser viajante, sua casa é o mundo inteiro. O signo viaja muito também em sua própria mente. Não tente aprisionar Sagitário de nenhuma forma, ele não aceita limites. Otimista e jovial, pode ter exageros e uma dose de deboche.",
+            "Capricórnio costuma ter seus propósitos e metas bem definidos. Signo de praticidade que segue as regras do jogo tem total noção dos seus limites. Capricórnio é maduro e bem humorado, mas também pode ser frio nas relações e um tanto pessimista.",
+            "Aquário costuma ter a amizade como cartão de visita e ter uma conexão intensa com a liberdade. Signo idealista, tende a dar preferência aos diálogos racionais. Aquário é solidário e humano, também pode ser radical e bem “do contra”.",
+            "Peixes costuma entender tudo sem precisar de muitas palavras, possuem uma sensibilidade superior. Signo compassivo e inspirador, Peixes tende a ter uma forte conexão com a espiritualidade, mas pode se iludir com certa facilidade."
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lista = (ListView) findViewById(R.id.lista);
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_expandable_list_item_1,
+                android.R.id.text1,
+                signos
+        );
+
+        lista.setAdapter(arrayAdapter);
+
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String posicao = arrayAdapter.getItem(position);
+
+                if (signos[0] == posicao){
+                    Toast.makeText(getApplicationContext(),descricao[0],Toast.LENGTH_LONG).show();
+                }
+                if (signos[1] == posicao){
+                    Toast.makeText(getApplicationContext(),descricao[1],Toast.LENGTH_LONG).show();
+                }
+                if (signos[2] == posicao){
+                    Toast.makeText(getApplicationContext(),descricao[2],Toast.LENGTH_LONG).show();
+                }
+                if (signos[3] == posicao){
+                    Toast.makeText(getApplicationContext(),descricao[3],Toast.LENGTH_LONG).show();
+                }
+                if (signos[4] == posicao){
+                    Toast.makeText(getApplicationContext(),descricao[4],Toast.LENGTH_LONG).show();
+                }
+                if (signos[5] == posicao){
+                    Toast.makeText(getApplicationContext(),descricao[5],Toast.LENGTH_LONG).show();
+                }
+                if (signos[6] == posicao){
+                    Toast.makeText(getApplicationContext(),descricao[6],Toast.LENGTH_LONG).show();
+                }
+                if (signos[7] == posicao){
+                    Toast.makeText(getApplicationContext(),descricao[7],Toast.LENGTH_LONG).show();
+                }
+                if (signos[8] == posicao){
+                    Toast.makeText(getApplicationContext(),descricao[8],Toast.LENGTH_LONG).show();
+                }
+                if (signos[9] == posicao){
+                    Toast.makeText(getApplicationContext(),descricao[9],Toast.LENGTH_LONG).show();
+                }
+                if (signos[10] == posicao){
+                    Toast.makeText(getApplicationContext(),descricao[10],Toast.LENGTH_LONG).show();
+                }
+                if (signos[11] == posicao){
+                    Toast.makeText(getApplicationContext(),descricao[11],Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+        });
+    }
+}
